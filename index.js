89b164ab2587509ee869137eba0c6b4b6d8b5999
alok <saxena>
2018-07-10 17:21:34 +0530

    simpleapp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var port = process.env.PORT || 3000;
 
