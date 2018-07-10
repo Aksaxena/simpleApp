61a607ad12aaa3c0eec66bd34057cdab77ee4f2c
alok <saxena>
2018-07-10 17:20:24 +0530

    simpleapp

--- index.js
+++ index.js
@@ -4,6 +4,37 @@ var io = require('socket.io')(http);
 var mysql = require('mysql');
 var port = process.env.PORT || 3000;
 
+
+//add body barser to accept post requests
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+//database connection with mysql
+
+var connection = mysql.createConnection({
+    host: 'puzzlecoder.com',
+    user: 'wil_user',
+    //password: 'notzft_$16@xyz',
+    password: 'x-8}OpOz6m1T',
+    //database: 'notsoforgetful_db'
+    database: 'vefinder'
+});
+
+// parse application/json 
+app.use(bodyParser.json());
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected ... ");
+    } else {
+        console.log("error connecting database ... ");
+    }
+});
+
+//start chat server
+server.listen(port);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
