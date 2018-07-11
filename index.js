0c48d500aba24e3890aa297540ef841b52152ef5
alok <saxena>
2018-07-11 11:38:18 +0530

    chatpopup original

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-var express = require('express'),
-        app = express(),
-        http = require('http'),
-        server = http.createServer(app),
-        io = require('socket.io').listen(server),
-        _ = require('underscore'),
-        moment = require('moment'),
-        request = require('request'),
-        //waterfall = require('async-waterfall'),
-        bodyParser = require('body-parser'),
-        usernames = {},
-        userIds = {},
-        clients = [],
-        users = [],
-        onlineClient = [],
-        clientInfo = {},
-        mysql = require('mysql'),
-        port = 3000;
+const app = require('express')()
+const server = require('http').createServer(app);
+const port = 3000;
 
-
-//add body barser to accept post requests
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-
-//database connection with mysql
-
-var connection = mysql.createConnection({
-    host: 'puzzlecoder.com',
-    user: 'wil_user',
-    //password: 'notzft_$16@xyz',
-    password: 'x-8}OpOz6m1T',
-    //database: 'notsoforgetful_db'
-    database: 'vefinder'
-});
-
-// parse application/json 
-app.use(bodyParser.json());
-
-connection.connect(function (err) {
-    if (!err) {
-        console.log("Database is connected ... ");
-    } else {
-        console.log("error connecting database ... ");
-    }
-});
-
-//start chat server
-server.listen(port);
+server.listen(port,  () => console.log(`Server is listening on ${port}`));
+io = require('socket.io')()
+io.listen(server)
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -57,8 +14,7 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+
